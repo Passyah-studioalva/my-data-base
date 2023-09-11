bb9c5daf64137c66e000e233bd85ec80d7f6ddae
Dinar Amalia <passyah@studioalva.co>
2023-09-11 23:55:21 +0700

    update

--- schemas/shopee.ts
+++ schemas/shopee.ts
@@ -41,34 +41,34 @@ export default {
     //     hotspot: true,
     //   },
     // },
-    // {
-    //   name: 'content',
-    //   title: 'Content',
-    //   type: 'array',
-    //   of: [
-    //     {type: 'block'},
-    //     {
-    //       type: 'image',
-    //       options: {
-    //         hotspot: true,
-    //       },
-    //       fields: [
-    //         {
-    //           name: 'caption',
-    //           type: 'string',
-    //           title: 'Image caption',
-    //           description: 'Caption displayed below the image.',
-    //         },
-    //         {
-    //           name: 'alt',
-    //           type: 'string',
-    //           title: 'Alternative text',
-    //           description: 'Important for SEO and accessiblity.',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Image caption',
+              description: 'Caption displayed below the image.',
+            },
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+              description: 'Important for SEO and accessiblity.',
+            },
+          ],
+        },
+      ],
+    },
     {
       name: 'date',
       title: 'Date',
