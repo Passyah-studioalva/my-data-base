7dfa23acbfa0f5ea65549b336db34be237c3c84b
Dinar Amalia <passyah@studioalva.co>
2023-09-11 22:28:05 +0700

    data

---
+++ schemas/index.ts
@@ -0,0 +1,4 @@
+/* Import Here */
+import shopee from "./shopee"
+
+export const schemaTypes = [shopee]
---
+++ schemas/shopee.ts
@@ -0,0 +1,72 @@
+// schemas/shopee.ts
+export default {
+  name: 'shopee',
+  type: 'document',
+  title: 'Shopee',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule: { required: () => any; }) => rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        isUnique: (value: any, context: { defaultIsUnique: (arg0: any, arg1: any) => any; }) => context.defaultIsUnique(value, context),
+      },
+      validation: (rule: { required: () => any; }) => rule.required(),
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Image caption',
+              description: 'Caption displayed below the image.',
+            },
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+              description: 'Important for SEO and accessiblity.',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    },
+  ],
+}
