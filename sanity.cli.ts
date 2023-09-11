453a99469b635cc16c818d25a0042ad9509a8a4b
Dinar Amalia <passyah@studioalva.co>
2023-09-12 00:49:14 +0700

    update

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'eeb4zajt',
+    projectId: 'iwjwzghi',
     dataset: 'production'
   }
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -5,9 +5,9 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'Sanity Project',
+  title: 'my-data-base',
 
-  projectId: 'eeb4zajt',
+  projectId: 'iwjwzghi',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
