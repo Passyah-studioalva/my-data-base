08cb38c80a75a3eba3f613229b2a9e45793fa018
Dinar Amalia <passyah@studioalva.co>
2023-09-17 19:24:47 +0700

    testing

--- schemas/testing.ts
+++ schemas/testing.ts
@@ -8,33 +8,20 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
-      validation: (rule: { required: () => any; }) => rule.required(),
+      validation: (rule: {required: () => any}) => rule.required(),
     },
     {
       name: 'number',
-      title: 'Title',
+      title: 'Number',
       type: 'number',
-      validation: (rule: { required: () => any; }) => rule.required(),
+      validation: (rule: {required: () => any}) => rule.required(),
     },
     {
       name: 'link',
       title: 'Link',
       type: 'string',
-      validation: (rule: { required: () => any; }) => rule.required(),
+      validation: (rule: {required: () => any}) => rule.required(),
     },
-    /* Slug */
-    // {
-    //   name: 'slug',
-    //   title: 'Slug',
-    //   type: 'slug',
-    //   options: {
-    //     source: 'title',
-    //     maxLength: 96,
-    //     isUnique: (value: any, context: { defaultIsUnique: (arg0: any, arg1: any) => any; }) => context.defaultIsUnique(value, context),
-    //   },
-    //   validation: (rule: { required: () => any; }) => rule.required(),
-    // },
-    /* Image */
     {
       name: 'coverImage',
       title: 'Cover Image',
@@ -43,40 +30,28 @@ export default {
         hotspot: true,
       },
     },
-    /* Content */
-    // {
-    //   name: 'content',
-    //   title: 'Content',
-    //   type: 'array',
-    //   of: [
-    //     {type: 'block'},
-    //     {
-    //       type: 'image',
-    //       options: {
-    //         hotspot: true,
-    //       },
-    //       fields: [
-    //         {
-    //           name: 'caption',
-    //           type: 'string',
-    //           title: 'Image caption',
-    //           description: 'Caption displayed below the image.',
-    //         },
-    //         {
-    //           name: 'alt',
-    //           type: 'string',
-    //           title: 'Alternative text',
-    //           description: 'Important for SEO and accessiblity.',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
     {
       name: 'date',
       title: 'Date',
       type: 'datetime',
       initialValue: () => new Date().toISOString(),
+    },
+  ],
+  orderings: [
+    {
+      title: 'Release Date, New',
+      name: 'releaseDateDesc',
+      by: [{field: 'releaseDate', direction: 'desc'}],
+    },
+    {
+      title: 'Release Date, Old',
+      name: 'releaseDateAsc',
+      by: [{field: 'releaseDate', direction: 'asc'}],
+    },
+    {
+      title: 'Popularity',
+      name: 'popularityDesc',
+      by: [{field: 'popularity', direction: 'desc'}],
     },
   ],
 }
