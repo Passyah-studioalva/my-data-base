29823d72d6437a2dfa6cbfe717645934f68c452a
Passyah Raffi <passyah@studioalva.co>
2023-09-21 14:06:10 +0700

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,9 +1,9 @@
 /* Import Here */
 import shopee from './shopee'
-import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import posts from './posts'
 import author from './author'
+import blockContent from './blockContent'
 
 export const schemaTypes = [shopee, post, posts, author, category, blockContent]
