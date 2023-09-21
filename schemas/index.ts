ec759a78a8f04155ef95cf0d616e7828a2bb2600
Passyah Raffi <passyah@studioalva.co>
2023-09-21 14:25:56 +0700

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,9 +1,8 @@
 /* Import Here */
 import shopee from './shopee'
-import category from './category'
-import post from './post'
-import posts from './posts'
+import passyahRaffi from './passyahRaffi'
 import author from './author'
+import category from './category'
 import blockContent from './blockContent'
 
-export const schemaTypes = [shopee, post, posts, author, category, blockContent]
+export const schemaTypes = [shopee, passyahRaffi, author, category, blockContent]
--- schemas/passyahRaffi.ts
+++ schemas/passyahRaffi.ts
@@ -1,10 +1,16 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'passyahRaffi',
   type: 'document',
+  title: 'PassyahRaffi',
   fields: [
+    defineField({
+      name: 'number',
+      title: 'Number',
+      type: 'number',
+      validation: (rule: {required: () => any}) => rule.required(),
+    }),
     defineField({
       name: 'title',
       title: 'Title',
@@ -40,24 +46,41 @@ export default defineType({
       of: [{type: 'reference', to: {type: 'category'}}],
     }),
     defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
       name: 'body',
       title: 'Body',
       type: 'blockContent',
     }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    }),
   ],
-
+  orderings: [
+    {
+      title: 'Number Old',
+      name: 'number',
+      by: [{field: 'number', direction: 'desc'}],
+    },
+    {
+      title: 'Number New',
+      name: 'number',
+      by: [{field: 'number', direction: 'asc'}],
+    },
+  ],
   preview: {
     select: {
       title: 'title',
       author: 'author.name',
       media: 'mainImage',
     },
-    prepare(selection) {
+    prepare(selection: {author: any}) {
       const {author} = selection
       return {...selection, subtitle: author && `by ${author}`}
     },
