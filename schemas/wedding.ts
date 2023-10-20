995cb77f9865080cca1a0174981ed3619d14277e
Passyah Raffi <passyah@studioalva.co>
2023-10-20 21:58:38 +0700

    update

--- schemas/wedding.ts
+++ schemas/wedding.ts
@@ -18,16 +18,8 @@ export default defineType({
       type: 'string',
     }),
     defineField({
-      name: 'coverIcon',
-      title: 'Icon utama',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
       name: 'husbandWifeNickname',
-      title: 'Nama Panggilan Pria & Wanita',
+      title: 'Nama Mempelai Pria & Wanita',
       type: 'string',
     }),
     defineField({
@@ -40,23 +32,6 @@ export default defineType({
       },
     }),
     defineField({
-      name: 'textKepada',
-      title: 'Text Ditujukan Kepada',
-      type: 'text',
-    }),
-    defineField({
-      name: 'buttonTextOpen',
-      title: 'Button Text Buka Undangan',
-      type: 'string',
-    }),
-    /* SECTION 1 */
-    defineField({
-      name: 'quotes',
-      title: 'Quotes',
-      type: 'text',
-    }),
-    /* SECTION 2 */
-    defineField({
       name: 'nameHusband',
       title: 'Nama Lengkap Mempelai Pria',
       type: 'string',
@@ -64,7 +39,8 @@ export default defineType({
     defineField({
       name: 'descHusband',
       title: 'Deskripsi Mempelai Pria',
-      type: 'text',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
       name: 'nameWife',
@@ -74,138 +50,48 @@ export default defineType({
     defineField({
       name: 'descWife',
       title: 'Deskripsi Mempelai Wanita',
-      type: 'text',
-    }),
-    /* SECTION 3 */
-    defineField({
-      name: 'subtitleAkad',
-      title: 'Subtitle Akad',
-      type: 'string',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
       name: 'descAkad',
-      title: 'Deskripsi Akad',
-      type: 'text',
-    }),
-    defineField({
-      name: 'locationAkad',
-      title: 'Lokasi Akad',
-      type: 'string',
-    }),
-    defineField({
-      name: 'subtitleResepsi',
-      title: 'Subtitle Resepsi',
-      type: 'string',
+      title: 'Hari, Tanggal, Jam Akad, Lokasi',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
       name: 'descResepsi',
-      title: 'Deskripsi Resepsi',
-      type: 'text',
+      title: 'Hari, Tanggal, Jam Akad, Lokasi',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
-      name: 'locationResepsi',
-      title: 'Lokasi Resepsi',
-      type: 'string',
-    }),
-    /* SECTION 4 */
-    defineField({
-      name: 'subtitleMaps',
-      title: 'Subtitle Lokasi Acara',
+      name: 'linkMaps',
+      title: 'Link Google Maps',
       type: 'string',
     }),
     defineField({
-      name: 'iframeMaps',
-      title: 'I Frame Maps',
-      type: 'text',
+      name: 'linkIframe',
+      title: 'Link Iframe Maps',
+      type: 'string',
     }),
     defineField({
       name: 'detailLocation',
-      title: 'Detail Lokasi',
-      type: 'text',
+      title: 'Detail Lokasi Acara',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
-      name: 'buttonTextMaps',
-      title: 'Button Text Maps',
-      type: 'string',
-    }),
-    /* SECTION 5 */
-    defineField({
-      name: 'subtitleWeddingGift',
-      title: 'Text Wedding Gift',
-      type: 'string',
+      name: 'detailBankFirst',
+      title: 'BANK 1 - Nama, Atas Nama, No. Rekening',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
-      name: 'bank1',
-      title: 'Nama Bank 1',
-      type: 'string',
-    }),
-    defineField({
-      name: 'anBank1',
-      title: 'AN. Nama Bank 1',
-      type: 'string',
-    }),
-    defineField({
-      name: 'noRekBank1',
-      title: 'No. Rek. Bank 1',
-      type: 'string',
-    }),
-    defineField({
-      name: 'btnCopyBank1',
-      title: 'Button Copy Bank 1',
-      type: 'string',
-    }),
-    defineField({
-      name: 'bank2',
-      title: 'Nama Bank 2',
-      type: 'string',
-    }),
-    defineField({
-      name: 'anBank2',
-      title: 'AN. Nama Bank 2',
-      type: 'string',
-    }),
-    defineField({
-      name: 'noRekBank2',
-      title: 'No. Rek. Bank 2',
-      type: 'string',
-    }),
-    defineField({
-      name: 'btnCopyBank2',
-      title: 'Button Copy Bank 2',
-      type: 'string',
-    }),
-    /* SECTION 5 */
-    defineField({
-      name: 'subtitleForm',
-      title: 'Text Judul Formulir Tamu',
-      type: 'string',
-    }),
-    defineField({
-      name: 'descForm',
-      title: 'Text Deskripsi Formulir Tamu',
-      type: 'string',
-    }),
-    /* SECTION 5 */
-    defineField({
-      name: 'subtitleLast',
-      title: 'Text Judul Penutupan',
-      type: 'string',
-    }),
-    defineField({
-      name: 'descLast',
-      title: 'Text Deskripsi Penutupan',
-      type: 'string',
-    }),
-    defineField({
-      name: 'husbandWifeNicknameLast',
-      title: 'Nama Penutupan Panggilan Pria & Wanita',
-      type: 'string',
-    }),
-    /* -------------------- */
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: 'detailBankLast',
+      title: 'BANK 2 - Nama, Atas Nama, No. Rekening',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
       name: 'publishedAt',
@@ -228,9 +114,8 @@ export default defineType({
   ],
   preview: {
     select: {
-      title: 'title',
+      title: 'husbandWifeNickname',
       order: 'order',
-      media: 'coverIcon',
     },
     prepare(selection: {title: any; order: any}) {
       const {title, order} = selection
