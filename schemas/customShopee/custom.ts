7695062c6e23289acde1d1d9988d9cbf21ae971b
Dinar Amalia <passyah@studioalva.co>
2023-11-24 16:16:33 +0700

    update

--- schemas/customShopee/custom.ts
+++ schemas/customShopee/custom.ts
@@ -23,9 +23,29 @@ export default defineType({
     }),
     defineField({
       name: 'colorProduct',
-      title: 'Color Product ( #ffddee ) sumber https://simplecss.eu/rgbatohex.html',
+      title: 'Color Product ( rgb(255, 221, 238, 0.4) ) sumber https://www.w3schools.com/cssref/tryit.php?filename=trycss_color_rgba',
       type: 'string',
       validation: (rule: {required: () => any}) => rule.required(),
+    }),
+    defineField({
+      name: 'bgRepeat',
+      title: 'Background Repeat ( repeat ) sumber https://www.w3schools.com/cssref/pr_background-repeat.php',
+      type: 'string',
+    }),
+    defineField({
+      name: 'bgSize',
+      title: 'Background Size ( contain ) sumber https://www.w3schools.com/cssref/css3_pr_background-size.php',
+      type: 'string',
+    }),
+    defineField({
+      name: 'scrollSnapAlign',
+      title: 'Scroll Snap Align ( center ) sumber https://www.w3schools.com/cssref/css_pr_scroll-snap-align.php',
+      type: 'string',
+    }),
+    defineField({
+      name: 'copyright',
+      title: 'copyright ( ©2023 Racun Shopee )',
+      type: 'string',
     }),
     defineField({
       name: 'date',
