63a42e97160fe2a5e6d803603e1ee1d888143171
Passyah Raffi <passyah@studioalva.co>
2024-01-03 15:54:29 +0700

    add new key

--- schemas/3-PASSYAH-WEB/project.ts
+++ schemas/3-PASSYAH-WEB/project.ts
@@ -24,6 +24,11 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'bgColorImg',
+      title: 'Background Image Color',
+      type: 'string',
+    }),
+    defineField({
       name: 'bgColor',
       title: 'Background Color',
       type: 'string',
